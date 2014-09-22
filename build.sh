a8f503147f78cef0d8d98fb79855d20a1050c6ce
Robert Nelson <robertcnelson@gmail.com>
2014-09-22 10:55:35 -0500

    add basic build script
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-make ARCH=arm CROSS_COMPILE= LINUX_SRC=/build/buildd/linux-src all
+ARCH=$(uname -m)
+
+x86_dir="/opt/github/ti-linux-kernel-dev"
+x86_compiler="gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin/arm-linux-gnueabihf-"
+
+if [ "x${ARCH}" = "xarmv7l" ] ; then
+	make ARCH=arm CROSS_COMPILE= LINUX_SRC=/build/buildd/linux-src all
+else
+	make ARCH=arm CROSS_COMPILE=${x86_dir}/dl/${x86_compiler} LINUX_SRC=${x86_dir}/KERNEL all
+fi
+#
