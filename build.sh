5793657a8519a2916ea07c2a148d4074cef04540
Robert Nelson <robertcnelson@gmail.com>
2014-09-22 16:21:23 -0500

    build: generic
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ x86_dir="/opt/github/ti-linux-kernel-dev"
 x86_compiler="gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin/arm-linux-gnueabihf-"
 
 if [ "x${ARCH}" = "xarmv7l" ] ; then
-	make ARCH=arm CROSS_COMPILE= LINUX_SRC=/build/buildd/linux-src all
+	make_options="CROSS_COMPILE= LINUX_SRC=/build/buildd/linux-src"
 else
-	make ARCH=arm CROSS_COMPILE=${x86_dir}/dl/${x86_compiler} LINUX_SRC=${x86_dir}/KERNEL all
+	make_options="CROSS_COMPILE=${x86_dir}/dl/${x86_compiler} LINUX_SRC=${x86_dir}/KERNEL"
 fi
+
+make ARCH=arm ${make_options} all
 #
